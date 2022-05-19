e131411cea99a7c23183ffa28d67fb3a8144bfcb
Rahul <rahulmalik1995@gmail.com>
2022-05-19 07:27:12 +0000

    added base spring app

---
+++ signup-app/src/main/java/com/rahul/registration/signupapp/SignupAppApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.registration.signupapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupAppApplication.class, args);
+	}
+
+}
---
+++ signup-app/src/test/java/com/rahul/registration/signupapp/SignupAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.registration.signupapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
